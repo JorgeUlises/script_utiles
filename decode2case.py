89a5979e4843cff655b71961fece5d7b62005a9f
Jorge Ulises Useche Cuellar <juuseche@udistrital.edu.co>
2015-05-13 15:34:00 -0500

    Cambios para hacer que la función decode tenga más términos de entrada

--- decode2case.py
+++ decode2case.py
@@ -1,19 +1,35 @@
 #!/usr/bin/python
+def convertir(line):
+    for i in caracteres:
+       line = line.replace(i,'')
+    par = line.split(',')
+    #print dir(par)
+    if len(par)<=5:
+        sql = (
+            "(CASE WHEN %s = %s THEN %s"
+                  "WHEN %s = %s THEN %s END)" 
+            % (par[0],par[1],par[2],par[0],par[3],par[4])
+        )
+    else:
+        sql = "(CASE WHEN {} = {} THEN {} " 
+        for i in range(0,(len(par)- 5)/2):
+            sql += "WHEN {} = {} THEN {} "
+        sql += "WHEN {} = {} THEN {} END)"
+        lista = []
+        for i in range(1,((len(par)-1)/2)+1):
+            lista.extend([par[0],par[(i*2)-1],par[i*2]])
+        #print lista
+        #print sql
+        #print len(lista)
+        #print len(sql.split("{}"))-1
+        sql = sql.format(*lista)
+    print sql
+
 fname = "sql.txt"
+caracteres = ['decode(','DECODE(',')']
 with open(fname) as f:
     content = f.read().splitlines()
-#print content
-line = content[0]
-caracteres = ['decode(',')']
-for i in caracteres:
-   line = line.replace(i,'')
 
-par = line.split(',')
+for line in content:
+    convertir(line)
 
-sql = (
-"(CASE WHEN %s = %s THEN %s"
-     "WHEN %s = %s THEN %s END)" 
-% (par[0],par[1],par[2],par[0],par[3],par[4])
-)
-
-print sql
