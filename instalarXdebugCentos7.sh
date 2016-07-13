4019a0784db9ece7db364460e5eec068acac023c
Jorge Useche <naturalmentejorge@gmail.com>
2016-07-13 15:56:15 -0500

    Este script instala Xdebug en apache-php en centos7

---
+++ instalarXdebugCentos7.sh
@@ -0,0 +1,12 @@
+wget http://xdebug.org/files/xdebug-2.4.0.tgz
+tar -xvzf xdebug-*
+cd xdebug-*
+yum install -y php-devel
+phpize
+./configure
+make
+cp modules/xdebug.so /usr/lib64/php/modules
+cat <<'EOF' > /etc/php.ini
+zend_extension = /usr/lib64/php/modules/xdebug.so
+EOF
+echo Xdebug instalado
