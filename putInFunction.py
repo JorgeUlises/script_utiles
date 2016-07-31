2a60673f927f0e96786f1298fb3b04440c676cf5
Jorge Ulises Useche Cuellar <naturalmentejorge@gmail.com>
2016-07-31 12:21:07 -0500

    Script para generar textos con funciones a partir de un patrón

---
+++ putInFunction.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+import sys
+
+tipo = str(sys.argv[1])
+
+lines = ""
+def prompt_user():
+    global lines
+    global tipo
+    print 'Type Intro to end.'
+    while (True):
+        line = raw_input('> ')
+        if line == '':
+            print lines
+            lines = ""
+            #sys.exit()
+        if tipo == "c":
+            lines += "echo codificar('" + line + "').'<br>';\n"
+        else:
+            lines += "echo " + tipo  + "('" + line + "').'<br>';\n"
+
+if __name__ == "__main__":
+    prompt_user()
+
