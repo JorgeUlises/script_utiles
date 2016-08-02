435db63de114afc7c15f787df9db4c5f252a050f
Jorge Useche <naturalmentejorge@gmail.com>
2016-08-02 16:00:09 -0500

    --level=symfony

--- codificadorBasico.php
+++ codificadorBasico.php
@@ -1,20 +1,27 @@
 <?php
-function decodificar_url($cadena){       
-    $cadena=base64_decode(str_pad(strtr($cadena, '-_', '+/'), strlen($cadena) % 4, '=', STR_PAD_RIGHT)); 
+
+/**
+ * PHP Coding Standards Fixer: php-cs-fixer --level=symfony
+ */
+
+function decodificar_url($cadena)
+{
+    $cadena = base64_decode(str_pad(strtr($cadena, '-_', '+/'), strlen($cadena) % 4, '=', STR_PAD_RIGHT));
+
     return cadena;
 }
 
-function decodificar($cadena){      
-    $cadena=strrev($cadena);
-    $cadena=base64_decode($cadena);
-    
+function decodificar($cadena)
+{
+    $cadena = strrev($cadena);
+    $cadena = base64_decode($cadena);
+
     return $cadena;
 }
 
-function codificar($cadena){
-return mcrypt_decrypt($cadena);
-
+function codificar($cadena)
+{
+    return mcrypt_decrypt($cadena);
 }
-echo decodificar("EHMaXsjbOvAF-oXohX5H1m1RYi48qotnlBIyLtAdSus");
-echo codificar("EHMaXsjbOvAF-oXohX5H1m1RYi48qotnlBIyLtAdSus");
-?>
+echo decodificar('EHMaXsjbOvAF-oXohX5H1m1RYi48qotnlBIyLtAdSus');
+echo codificar('EHMaXsjbOvAF-oXohX5H1m1RYi48qotnlBIyLtAdSus');
