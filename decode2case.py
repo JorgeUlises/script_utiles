304e296a74913ca145ae472b09c60fea762d597e
Jorge Useche <naturalmentejorge@gmail.com>
2016-08-02 16:06:10 -0500

    Autopep8

--- decode2case.py
+++ decode2case.py
@@ -1,124 +1,135 @@
 #!/usr/bin/python
+
+# Auto pep8: autopep8 --in-place --aggressive --aggressive
+
 def findComma(line):
     fin = ''
     if line.find('),') > -1 or line.find(') ,') > -1:
         fin = ','
     return fin
 
-def replaceChars(line,caracteres):
+
+def replaceChars(line, caracteres):
     for i in caracteres:
-        line = line.replace(i,'')
+        line = line.replace(i, '')
     return line
 
-def convDecode(line,caracteres):
+
+def convDecode(line, caracteres):
     fin = findComma(line)
-    line = replaceChars(line,caracteres)
+    line = replaceChars(line, caracteres)
     par = line.split(',')
-    #print par
-    #print dir(par)
-    if len(par)==4:
+    # print par
+    # print dir(par)
+    if len(par) == 4:
         sql = (
             "(CASE WHEN %s = %s THEN %s "
-                  "ELSE %s END)" 
-            % (par[0],par[1],par[2],par[3])
+            "ELSE %s END)"
+            % (par[0], par[1], par[2], par[3])
         )
-    elif len(par)==5:
+    elif len(par) == 5:
         sql = (
             "(CASE WHEN %s = %s THEN %s "
-                  "WHEN %s = %s THEN %s END)" 
-            % (par[0],par[1],par[2],par[0],par[3],par[4])
+            "WHEN %s = %s THEN %s END)"
+            % (par[0], par[1], par[2], par[0], par[3], par[4])
         )
     else:
-        sql = "(CASE WHEN {} = {} THEN {} " 
-        for i in range(0,(len(par)- 5)/2):
+        sql = "(CASE WHEN {} = {} THEN {} "
+        for i in range(0, (len(par) - 5) / 2):
             sql += "WHEN {} = {} THEN {} "
         lista = []
-        for i in range(1,((len(par)-1)/2)+1):
-            lista.extend([par[0],par[(i*2)-1],par[i*2]])
-        #print lista
-        if len(par) % 2 == 0:#par
-            lista.extend([par[len(par)-1]])
-            #print lista
+        for i in range(1, ((len(par) - 1) / 2) + 1):
+            lista.extend([par[0], par[(i * 2) - 1], par[i * 2]])
+        # print lista
+        if len(par) % 2 == 0:  # par
+            lista.extend([par[len(par) - 1]])
+            # print lista
             sql += "WHEN {} = {} THEN {} ELSE {} END)" + fin
         else:
             sql += "WHEN {} = {} THEN {} END)" + fin
-        #print lista
-        #print sql
-        #print len(lista)
-        #print len(sql.split("{}"))-1
+        # print lista
+        # print sql
+        # print len(lista)
+        # print len(sql.split("{}"))-1
         sql = sql.format(*lista)
     print sql
 
-def convNVL(line,caracteres):
-    fin	= findComma(line)
-    line = replaceChars(line,caracteres)
+
+def convNVL(line, caracteres):
+    fin = findComma(line)
+    line = replaceChars(line, caracteres)
     par = line.split(',')
-    if len(par)==2:
+    if len(par) == 2:
         sql = (
-            "COALESCE(%s,%s)"  
-            % (par[0],par[1])
+            "COALESCE(%s,%s)"
+            % (par[0], par[1])
         )
-       	sql += fin
+        sql += fin
     print sql
 
-def convLPAD(line,caracteres):
-    fin	= findComma(line)
-    line = replaceChars(line,caracteres)
+
+def convLPAD(line, caracteres):
+    fin = findComma(line)
+    line = replaceChars(line, caracteres)
     par = line.split(',')
-    if len(par)==3:
+    if len(par) == 3:
         sql = (
-            "LPAD(CAST(%s AS TEXT),%s,'%s')"  
-            % (par[0],par[1],par[2])
+            "LPAD(CAST(%s AS TEXT),%s,'%s')"
+            % (par[0], par[1], par[2])
         )
-       	sql += fin
+        sql += fin
     print sql
 
-def convTRIM(line,caracteres):
-    fin	= findComma(line)
-    line = replaceChars(line,caracteres)
+
+def convTRIM(line, caracteres):
+    fin = findComma(line)
+    line = replaceChars(line, caracteres)
     par = line.split(',')
-    if len(par)==3:
+    if len(par) == 3:
         sql = (
-            "LPAD(CAST(%s AS TEXT),%s,'%s')"  
-            % (par[0],par[1],par[2])
+            "LPAD(CAST(%s AS TEXT),%s,'%s')"
+            % (par[0], par[1], par[2])
         )
-       	sql += fin
+        sql += fin
     print sql
 
-def convTO_CHAR(line,caracteres):
-    fin	= findComma(line)
-    line = replaceChars(line,caracteres)
+
+def convTO_CHAR(line, caracteres):
+    fin = findComma(line)
+    line = replaceChars(line, caracteres)
     par = line.split(',')
-    if len(par)==1:
+    if len(par) == 1:
         sql = (
-            "CAST(%s AS TEXT)"  
+            "CAST(%s AS TEXT)"
             % (par[0])
         )
-       	sql += fin
+        sql += fin
     print sql
 
-def convSUBSTR(line,caracteres):
-    fin	= findComma(line)
-    line = replaceChars(line,caracteres)
+
+def convSUBSTR(line, caracteres):
+    fin = findComma(line)
+    line = replaceChars(line, caracteres)
     par = line.split(',')
-    if len(par)==3:
+    if len(par) == 3:
         sql = (
-            "SUBSTR(CAST(%s AS TEXT),%s,%s)"  
-            % (par[0],par[1],par[2])
+            "SUBSTR(CAST(%s AS TEXT),%s,%s)"
+            % (par[0], par[1], par[2])
         )
-       	sql += fin
+        sql += fin
     print sql
 
-def convLENGTH(line,caracteres):
-    fin	= findComma(line)
-    line = replaceChars(line,caracteres)
+
+def convLENGTH(line, caracteres):
+    fin = findComma(line)
+    line = replaceChars(line, caracteres)
     par = line.split(',')
-    if len(par)==1:
+    if len(par) == 1:
         sql = (
-            "CHARACTER_LENGTH(CAST(%s AS TEXT))"  
+            "CHARACTER_LENGTH(CAST(%s AS TEXT))"
             % (par[0])
         )
-       	sql += fin
+        sql += fin
     print sql
 
 fname = "sql.txt"
@@ -127,30 +138,22 @@ with open(fname) as f:
 
 for line in content:
     if 'DECODE' in line.upper():
-   	caracteres = ['DECODE(','Decode(','decode(',')']
-        convDecode(line,caracteres)
+        caracteres = ['DECODE(', 'Decode(', 'decode(', ')']
+        convDecode(line, caracteres)
     elif 'NVL' in line.upper():
-        caracteres = ['NVL(','Nvl(','nvl(',')']
-        convNVL(line,caracteres)
+        caracteres = ['NVL(', 'Nvl(', 'nvl(', ')']
+        convNVL(line, caracteres)
     elif 'LPAD' in line.upper():
-        caracteres = ['LPAD(','Lpad(','lpad(',')']
-        convLPAD(line,caracteres)
+        caracteres = ['LPAD(', 'Lpad(', 'lpad(', ')']
+        convLPAD(line, caracteres)
     elif 'TO_CHAR' in line.upper():
-        caracteres = ['TO_CHAR(','To_char(','to_char(',')']
-        convTO_CHAR(line,caracteres)
+        caracteres = ['TO_CHAR(', 'To_char(', 'to_char(', ')']
+        convTO_CHAR(line, caracteres)
     elif 'SUBSTR' in line.upper():
-        caracteres = ['SUBSTR(','Substr(','substr(',')']
-        convSUBSTR(line,caracteres)
+        caracteres = ['SUBSTR(', 'Substr(', 'substr(', ')']
+        convSUBSTR(line, caracteres)
     elif 'LENGTH' in line.upper():
-        caracteres = ['LENGTH(','Length(','length(',')']
-        convLENGTH(line,caracteres)
+        caracteres = ['LENGTH(', 'Length(', 'length(', ')']
+        convLENGTH(line, caracteres)
     else:
         print "No more lines"
-
-
-
-
-
-
-
-
