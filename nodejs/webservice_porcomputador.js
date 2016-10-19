07d4510f7ce9f23a19b15bfbc702d5db498dcd35
Jorge Ulises Useche Cuellar <naturalmentejorge@gmail.com>
2016-10-19 18:43:40 -0500

    Se agrega ejemplo de como consumir el servicio

--- nodejs/webservice_porcomputador.js
+++ nodejs/webservice_porcomputador.js
@@ -1,10 +1,10 @@
-var request = require('request');
-var cheerio = require('cheerio');
+var request = require('request')
+var cheerio = require('cheerio')
 var http = require('http')
 var url = require('url')
 
 function getFirstTable(htmlSource) {
-  var $ = cheerio.load(htmlSource);
+  var $ = cheerio.load(htmlSource)
 
   var table = $("table:nth-child(1)")
     //var table2 = $("table:nth-child(2)")
@@ -12,16 +12,19 @@ function getFirstTable(htmlSource) {
   return table.html()
 }
 
-//para acceder: curl localhost:9999/?opcion=calcular
+//para acceder: localhost:9999/?opcion=calcular&limite=10,&Pesos=3,1,2,3,&Valores=4,5,6,
+console.log('Escuchando...  curl "http://localhost:9999/?opcion=calcular&limite=30,&Pesos=3,1,2,3,&Valores=4,5,6,"')
 http.createServer(function(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
   var url_parts = url.parse(req.url, true)
   var query = url_parts.query
   console.log(query); //{Object}
   if (query.opcion === 'calcular') {
     var postData = {
-      'limite': '10,',
-      'Pesos': '3,1,2,3,',
-      'Valores': '4,5,6,'
+      'limite': query.limite, //Peso límite
+      'Pesos': query.Pesos, //#artículos, 3 valores de pesos
+      'Valores': query.Valores //3 valores de valores
     }
     request.post({
       url: 'http://porcomputador.com/Perl/mochila2.php', //?limite=10&Pesos=3,1,2,3&Valores=4,5,6
